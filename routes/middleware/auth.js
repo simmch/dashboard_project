3c2cf010b522d1bd6b45154db4a83db5e8678bd2
Christopher Simmons <christopher.simmons@nike.com>
2020-01-24 23:02:38 -0800

    First Commit

---
+++ routes/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+  // Get Token from header
+  const token = req.header("x-auth-token");
+
+  // Check if no Token
+  if (!token) return res.status(401).json({ msg: "No Token. Auth Denied." });
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
---
+++ routes/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// user is the name of the model
+// UserSchema is the schema created above
+module.exports = User = mongoose.model("user", UserSchema);
